54a0f09998716ccace6cfa55ac899d31aaa5a306
Lape15 <larape1993@gmail.com>
2020-06-15 16:47:43 +0100

    filter implemented

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -6,6 +6,7 @@ const Homepage = () => {
   const [products, setProducts] = useState(null);
   const [brand, setBrand] = useState("");
   const [productType, setProductType] = useState("");
+  const [filterToggle, setFilterToggle] = useState(false);
   let makeUp;
   useEffect(() => {
     axios
@@ -20,12 +21,10 @@ const Homepage = () => {
     makeUp = products;
   }
 
-  // const handleChange = e => {
-  //   setForgotPassword({
-  //     ...forgotPassword,
-  //     [e.target.name]: e.target.value
-  //   })
-  // }
+  const handleFilter = () => {
+    setFilterToggle(!filterToggle);
+    console.log(filterToggle);
+  };
 
   const handleChange = (e) => {
     setBrand(e.target.value);
@@ -33,49 +32,76 @@ const Homepage = () => {
   };
   const onTypeChange = (e) => {
     setProductType(e.target.value);
-    console.log(productType);
+    // console.log(productType);
   };
 
   let brandData;
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-    try {
-      brandData = await axios.get(
-        `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brand}`
-      );
-      if (brandData.data) {
-        console.log(brandData.data);
-        // makeUp = brandData.data;
-        setProducts(brandData.data);
-        console.log(products);
+    if (brand.length > 0 && productType.length === 0) {
+      try {
+        brandData = await axios.get(
+          `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brand}`
+        );
+        if (brandData.data) {
+          setProducts(brandData.data);
+        }
+        // console.log(Array.isArray(brandData.data));
+      } catch (error) {
+        console.log(error);
       }
-      console.log(Array.isArray(brandData.data));
-    } catch (error) {
-      console.log(error);
     }
+
+    if (brand.length > 0 && productType.length > 0) {
+      try {
+        brandData = await axios.get(
+          `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brand}&product_type=${productType}`
+        );
+        if (brandData.data) {
+          setProducts(brandData.data);
+        }
+        // console.log(Array.isArray(brandData.data));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    setBrand("");
+    setProductType("");
   };
-  // useEffect(() => {
-  //   if (brandData) setProducts(brandData.data);
-  //   console.log(products);
-  // }, [brandData, products]);
+
+  const resetFilter = () => {
+    console.log(products);
+    // setProductType(products);
+  };
   return (
     <div className="home">
-      <div className="filter">
-        <div className="filter-btn">
+      <div className={`filter ${filterToggle ? "filter extra" : "filter"} `}>
+        <div className="filter-btn" onClick={handleFilter}>
           {" "}
           <i className="fas fa-filter"></i>Filters
         </div>
-        <div className="filter-item">
+        <div className={`hide ${filterToggle ? " filter-item" : "hide"}`}>
           <form onSubmit={handleFormSubmit}>
             <label>Brand</label>
             <input type="text" value={brand} onChange={handleChange} />
             <label>Product Type</label>
             <input type="text" value={productType} onChange={onTypeChange} />
-            <button>Filter</button>
+            <i class="fas fa-search"></i>
           </form>
+          <div className="reset-filter">
+            <button className="btn" onClick={resetFilter}>
+              Reset
+            </button>
+          </div>
         </div>
       </div>
-      {!products ? <p>Loading for shit </p> : <Card list={makeUp} />}
+      {!products ? (
+        <p>Loading for shit </p>
+      ) : products.length === 0 ? (
+        <p>Item not available</p>
+      ) : (
+        <Card list={makeUp} />
+      )}
     </div>
   );
 };
